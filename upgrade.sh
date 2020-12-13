f7827596c6a5d910ed16602bdb35b8b2ff0a431e
Björn Magnusson <bjornmagnusson1984@gmail.com>
2020-12-13 12:09:59 +0100

    Upgrade: added image reflector

--- upgrade.sh
+++ upgrade.sh
@@ -1,7 +1,11 @@
 VERSION=$1
 
 echo "Upgrading Flux2 toolkit to $VERSION"
-flux install --arch arm --version=$VERSION --components-extra image-automation-controller --export > arm/flux-system/gotk-components.yaml
+flux install \
+--arch arm \
+--version=$VERSION \
+--components-extra image-reflector-controller.image-automation-controller \
+--export > arm/flux-system/gotk-components.yaml
 
 git add arm/flux-system/gotk-components.yaml
 git commit -m "Flux: upgrade to $VERSION"
