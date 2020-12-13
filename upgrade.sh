3dfcac789fda7fb2b3bb371174e091bd86b894a6
Björn Magnusson <bjornmagnusson1984@gmail.com>
2020-12-13 11:50:47 +0100

    Upgrade: added image automation

--- upgrade.sh
+++ upgrade.sh
@@ -1,7 +1,7 @@
 VERSION=$1
 
 echo "Upgrading Flux2 toolkit to $VERSION"
-flux install --arch arm --version=$VERSION --export > arm/flux-system/gotk-components.yaml
+flux install --arch arm --version=$VERSION -components-extra image-automation-controller --export > arm/flux-system/gotk-components.yaml
 
 git add arm/flux-system/gotk-components.yaml
 git commit -m "Flux: upgrade to $VERSION"
